8ac36e25d58c09ba597690176da9ffbecfa1fc8a
vitpinheiro <120499203+vitpinheiro@users.noreply.github.com>
2023-05-28 01:26:41 +0000

    poo

--- Poo01/triangulo.py
+++ Poo01/triangulo.py
@@ -1,7 +1,9 @@
 class triangulo:
-  def __init__(self):
+  def __init__(self,b,h):
     self.__b=0
     self.__h=0
+    self.set_base(b)
+    self.set_altura(h)
   def set_base(self,v):
     if v>=0:self.__b=v
     else: raise valueError()
@@ -14,17 +16,23 @@ class triangulo:
     return self.__h
   def calc_area(self):
     return self.__b * self.__h / 2
+  def __str__(self):
+    return f"Base = {self.__b} - Altura = {self.__h}"
 
 class UI:
   @staticmethod
   def main():
-    x=triangulo()
-    print("Digite o valor da base")
+    b=float(input("Digite o valor da base \n"))
+    h=float(input("Digite o valor da altura \n"))
+    x=triangulo(b,h)
+    """print("Digite o valor da base")
     x.b=float(input())
     x.set_base(x.b)
     print("Digite o valor da altura")
     x.h=float(input())
-    x.set_altura(x.h)
-    print(f"A área do triângulo é {x.calc_area()}")
-
+    x.set_altura(x.h)"""
+    print(x)
+    print(f"A área do triângulo é: {x.calc_area()}")
+   
+   
 UI.main()
